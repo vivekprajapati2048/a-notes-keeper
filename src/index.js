8e4a4d9d20de7bff0d6e0b79bb363ff0fa6185fc
Vivek Prajapati <vivekprajapati2048@gmail.com>
2021-10-23 14:54:35 +0530

    keeper v2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import  App from "./components/App.jsx";
+import App from "./components/App";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);
-
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
+ReactDOM.render(<App />, document.getElementById("root"));
